cf4345b7d13801d8e8a02165d80d96a5d6d8eb36
Jayden <info@jethertechnology.net.ng>
2024-10-15 07:01:25 +0100

    Work was modified

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
 import React from "react";
-import  { Tilt } from "react-tilt";
+import  { 
+  Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
